0fd81f072a09da12a55387120757a4f63cf71300
dmandrews <danielmandrews@gmail.com>
2013-12-06 16:45:18 -0700

    Initial add
    added these files for HVRelays library

---
+++ HVRelays.h
@@ -0,0 +1,34 @@
+/*
+ *	HVRelays.h
+ *	Provides functions for safely turning on and off the relays
+ *	Functions make sure only one relay at a time is on for each
+ *		of the three ports. Delays are also given to allow the relay to open before the other closes.
+ *
+ *	EE490 - Microfluidics Analysis (Fall 2013)
+ *	Daniel Andrews
+ */
+
+#ifndef HVRelays_h
+#define HVRelays_h
+
+#include "Arduino.h"
+
+class HVRelays
+{
+	public:
+		HVRelays(); //constructor
+		void allOff();
+		void flowBuffer();
+		void loadSample();
+		void flowSample();
+		
+	private:
+		void relay1Enable();
+		void relay2Enable();
+		void relay3Enable();
+		void relay4Enable();
+		void relay5Enable();
+		void relay6Enable();
+};
+
+#endif
